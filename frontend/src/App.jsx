670ef1d859889a1d16eb92aa8386679be99c81e6
akpirate717 <akpirate717@gmail.com>
2025-04-14 20:31:29 +0530

    Dashboard

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import './index.css'
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return <Dashboard />;
+}
+
+export default App;
